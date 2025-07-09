0f53678062e01a9a3ded0bcf70704fe97a6b668a
Subhanisk <150007923+subhani12389@users.noreply.github.com>
2025-07-09 14:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.85%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+    int len=s.length();
+        for(int i = 0; i < len; i++) {
+            if(s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] + 32;
+        }
+    }   
+        return s;
+    }
+};
