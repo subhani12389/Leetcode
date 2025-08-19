485609212c60ad321f31b28ba47797d57bb3c6e7
Subhanisk <150007923+subhani12389@users.noreply.github.com>
2025-08-19 14:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (96.49%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     string addBinary(string s1, string s2) {
-        string ans = "";
-        int i = s1.size() - 1;
-        int j = s2.size() - 1;
-        int carry = 0;
+       int i=s1.size()-1;
+       int j=s2.size()-1;
+       int carry=0;
+       string ans="";
+       while(i>=0 || j>=0 || carry){
+        int bit1=(i>=0 ? s1[i]-'0' :0);
+        int bit2=(j>=0 ? s2[j]-'0' :0);
+        int sum=bit1+bit2+carry;
+        ans.push_back(sum%2+'0');
+        carry=sum/2;
+        i--;
+        j--;
 
-        while (i >= 0 || j >= 0 || carry) {
-            int bit1 = (i >= 0 ? s1[i] - '0' : 0);
-            int bit2 = (j >= 0 ? s2[j] - '0' : 0);
-
-            int sum = bit1 + bit2 + carry;
-
-            ans.push_back((sum % 2) + '0');
-            carry = sum / 2;
-
-            i--;
-            j--;
-        }
-
-        reverse(ans.begin(), ans.end());
-
+       }
+       reverse(ans.begin(),ans.end());
         return ans;
     }
 };
