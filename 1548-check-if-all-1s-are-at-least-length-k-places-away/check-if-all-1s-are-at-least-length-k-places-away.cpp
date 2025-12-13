752b540b57333c7c41a685458f6dde7c6005f2c2
Subhanisk <150007923+subhani12389@users.noreply.github.com>
2025-12-13 12:50:30 +0530

    Time: 0 ms (100.00%) | Memory: 61.6 MB (8.98%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int copy = k;   // initialize so first 1 doesn't fail
-        int count = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                if (copy < k) {
+        int copy=k;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                if(copy<k){
                     return false;
                 }
-                copy = 0;   // reset distance after seeing 1
-            } else {
-                copy++;     // count zeros between 1s
+                copy=0;
+            }
+            else{
+                copy++;
             }
         }
         return true;
